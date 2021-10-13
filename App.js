e3d59710d4dab3d333a0293bd28a9bf097aff77b
Gemma Teixidó <gemmateixido25@gmail.com>
2021-10-13 16:30:51 +0200

    Add View and Text for title Today's tasks

--- App.js
+++ App.js
@@ -5,8 +5,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+      
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+      
+        <View style={styles.items}>
+
+        </View>
+      
+      </View>
     </View>
   );
 }
